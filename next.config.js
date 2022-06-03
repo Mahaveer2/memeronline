5e6d820a536a1548049a1638ccdac87d0f38e57e
Partab Rai <partabrai@Partabs-MacBook-Pro.local>
2022-06-03 15:22:36 +0500

    2dsda

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ignoreDuringBuilds: true,
-  cssLoaderOptions: {
-    url: false
-  },
   images: {
     domains: ['icon-library.com',"rb.gy"],
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  distDir: 'build',
 };
 
 module.exports = nextConfig
